ab2d241c613d1a15e564e590f94c20c00991c748
yaniv231 <ynl1421@gmail.com>
2025-06-08 22:49:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# משתני סביבה - עם המזהה המתוקן
-BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN') or "7269747636:AAETblnIfIDN9kqH7vw8B6rdHVjM2_1ybrg"
+# משתני סביבה - עם הטוקן החדש
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN') or "7269747636:AAGSP-Nvm-C7bAiilqv7uO3hwvIrZhO3j58"
 CHANNEL_ID = os.getenv('CHANNEL_ID') or "-1007269747696"
 CHANNEL_USERNAME = os.getenv('CHANNEL_USERNAME') or "my_trading_channel"
 
